4f0b57b64da7715b2e08a0f3cb3c96fe95991020
Hri <hri.gh@outlook.com>
2026-01-16 11:48:35 +0530

    feat: Integrate admin and conversation routes into the Express application

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { errorMiddleware } from './middleware/error.middleware.js';
 import cors from 'cors'
-import testRouter from './routes/test.route.js'
+
 import { connectDB } from './config/db.js'
 
 const app = express();
@@ -16,11 +16,18 @@ app.use(cors({
 app.use(express.json())
 
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server is running!');
-});
+// app.get('/', (req: Request, res: Response) => {
+//     res.send('Express + TypeScript Server is running!');
+// });
+
+import testRouter from './routes/test.route.js'
+import adminRoutes from "./routes/admin.routes.js";
+import conversationRoutes from "./routes/conversation.routes.js";
 
 app.use('/api/test', testRouter)
+app.use("/api/admin", adminRoutes);
+app.use("/api/conversations", conversationRoutes);
+
 
 app.use(errorMiddleware)
 
