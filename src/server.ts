48d7a57656ffc1c7d9ab69a3897fef3ba6d0c39f
Hri <hri.gh@outlook.com>
2026-01-16 11:48:48 +0530

    feat: Set up HTTP server and initialize socket.io for real-time communication

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 // import 'dotenv/config'
+import http from 'http'
 import app from './app.js';
-
+import { initSocket } from './socket/index.js';
 
 const port = process.env.PORT || 3000;
 
+const server = http.createServer(app)
+
+// Initialize socket.io
+initSocket(server);
 
 
-app.listen(Number(port), '0.0.0.0', () => {
+server.listen(Number(port), '0.0.0.0', () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
