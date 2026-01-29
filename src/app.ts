d4b3dd060dc06985ae7476a3ae34f0ffd64033a5
Hri <hri.gh@outlook.com>
2026-01-30 01:25:04 +0530

    feat: update CORS configuration to support multiple allowed origins and enable credentials

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { errorMiddleware } from './middleware/error.middleware.js';
 import cors from 'cors'
+import cookieParser from 'cookie-parser';
 
 import { connectDB } from './config/db.js'
 
@@ -8,12 +9,32 @@ const app = express();
 
 connectDB()
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: false
-}))
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }))
+
+const allowedOrigins = process.env.CORS_ORIGINS?.split(",") || [];
+
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            // allow Postman / server-to-server
+            if (!origin) return callback(null, true);
+
+            if (allowedOrigins.includes(origin)) {
+                return callback(null, true);
+            }
+
+            return callback(new Error("Not allowed by CORS"));
+        },
+        credentials: true,
+    })
+);
+
 
 app.use(express.json())
+app.use(cookieParser())
 
 
 // app.get('/', (req: Request, res: Response) => {
@@ -21,12 +42,14 @@ app.use(express.json())
 // });
 
 import testRouter from './routes/test.route.js'
-import adminRoutes from "./routes/admin.routes.js";
-import conversationRoutes from "./routes/conversation.routes.js";
+import adminRouter from "./routes/admin.routes.js";
+import conversationRouter from "./routes/conversation.routes.js";
+import adminAuthRouter from "./routes/adminAuth.routes.js";
 
 app.use('/api/test', testRouter)
-app.use("/api/admin", adminRoutes);
-app.use("/api/conversations", conversationRoutes);
+app.use("/api/admin", adminRouter);
+app.use("/api/conversations", conversationRouter);
+app.use("/api/admin/auth", adminAuthRouter);
 
 
 app.use(errorMiddleware)
