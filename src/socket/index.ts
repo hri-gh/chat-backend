d231ed7db3dffe63236fd962a2184dbafe13f9d1
Hri <hri.gh@outlook.com>
2026-01-30 01:23:40 +0530

    feat: update CORS configuration to use allowed origins from environment variables

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,16 +2,40 @@ import { Server } from "socket.io";
 import { Server as HttpServer } from "http";
 import { registerVisitorSocket } from "./visitor.js";
 import { registerAdminSocket } from "./admin.js";
-
+import jwt from "jsonwebtoken";
 
 let io: Server;
 
 export function initSocket(server: HttpServer) {
+    // io = new Server(server, {
+    //     cors: {
+    //         origin: "*", // later restrict
+    //     },
+    // });
+
+    const allowedOrigins = process.env.CORS_ORIGINS?.split(",") || [];
+
     io = new Server(server, {
         cors: {
-            origin: "*", // later restrict
+            origin: allowedOrigins,
+            credentials: true,
         },
     });
+
+
+    // io.use((socket, next) => {
+    //     const token = socket.handshake.auth?.token;
+
+    //     if (!token) return next(new Error("Unauthorized"));
+
+    //     try {
+    //         const decoded = jwt.verify(token, process.env.ADMIN_JWT_SECRET!);
+    //         socket.data.admin = decoded;
+    //         next();
+    //     } catch {
+    //         next(new Error("Invalid token"));
+    //     }
+    // });
 
     io.on("connection", (socket) => {
         console.log("Socket connected:", socket.id);
